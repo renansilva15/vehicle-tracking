8df716a3f19383eee199160f77d95654b80a2974
renansilva15 <renangabrielsilva150@gmail.com>
2024-12-09 21:19:32 -0300

    chore(`backend/src/maps`): add places files

---
+++ backend/src/maps/places/places.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PlacesService {
+  // TODO: Type the return value
+  async findPlaces(text: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
